fbb8acc54590ab12e1c0fe88fa8aeade8a7b95d7
Tashvi26 <tashvi640@gmail.com>
2024-11-18 10:26:10 +0530

    netflix files

---
+++ day 10/netflix-clone/src/movies.js
@@ -0,0 +1,52 @@
+const movies=[
+    {
+    id:1,
+    image: "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/l/t/h/2022-blu-ray-20th-century-studios-english-avatar-2-original-imagvyqpzps5edgz.jpeg?q=70",
+    title: "avatar 2",
+    language: "english",
+    cost:1490,
+    },
+    {
+        id:2,
+        image: "https://rukminim2.flixcart.com/image/612/612/kph8h3k0/movie/w/x/l/2021-3d-blu-ray-warner-bros-excel-home-videos-english-justice-original-imag3ph6wufmqamk.jpeg?q=70",
+        title: "Justice Leaque",
+        language: "english",
+        cost:200,
+    },
+    {
+        id:3,
+        image: "https://rukminim2.flixcart.com/image/612/612/jg2kqkw0/movie/y/x/r/2017-dvd-sony-pictures-excel-home-videos-english-jumanji-welcome-original-imaf4d4tyfdmfude.jpeg?q=70",
+        title: "Jumangi",
+        language: "english",
+        cost:600,
+    },
+    {
+        id:4,
+        image: "https://rukminim2.flixcart.com/image/612/612/av-media/movies/j/g/7/xxx-the-next-level-original-imadg3cfzphnr3ed.jpeg?q=70",
+        title: "XXX",
+        language: "english",
+        cost:800,
+    },
+    {
+        id:5,
+        image: "https://rukminim2.flixcart.com/image/612/612/jbi93m80/movie/j/w/8/2009-dvd-excel-home-videos-english-harry-potter-and-the-half-original-imafytawreevzxp6.jpeg?q=70",
+        title: "Harry Potter",
+        language: "english",
+        cost:500,
+    },
+    {
+        id:6,
+        image: "https://rukminim2.flixcart.com/image/612/612/movie/v/4/d/morgan-2016-english-blu-ray-excel-home-videos-original-imaepfv5ttgfjpzc.jpeg?q=70",
+        title: "Morgan",
+        language: "english",
+        cost:300,
+    },
+    {
+        id:7,
+        image: "https://rukminim2.flixcart.com/image/612/612/xif0q/movie/s/g/j/2023-blu-ray-lionsgate-films-english-john-wick-chapter-4-original-imaguwy65nmhhaan.jpeg?q=70",
+        title: "John Wick 4",
+        language: "english",
+        cost:1000,
+    },
+];
+export default movies;
